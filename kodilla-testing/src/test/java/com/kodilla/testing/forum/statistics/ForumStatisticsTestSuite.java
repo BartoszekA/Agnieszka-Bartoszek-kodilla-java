6c4f063b1c45640f2c87a9f64b23731578de4358
BartoszekA <agnieszka.bartoszek@gmail.com>
2019-08-17 18:38:26 +0200

    statistics

--- kodilla-testing/src/test/java/com/kodilla/testing/forum/statistics/ForumStatisticsTestSuite.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/forum/statistics/ForumStatisticsTestSuite.java
@@ -1,7 +1,9 @@
 package com.kodilla.testing.forum.statistics;
 
+import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.mockito.Mockito.mock;
@@ -9,13 +11,30 @@ import static org.mockito.Mockito.when;
 
 public class ForumStatisticsTestSuite {
     @Test
-    public void testForumStatistics() {
+    public void testForumStatistics1() {
         //Given
         Statistics statisticsMock = mock(Statistics.class);
-        List<String> names = new List<String>();
+        List<String> names = new ArrayList<String>();
+        int posts = 0;
+        int comments = 0;
         when(statisticsMock.usersNames()).thenReturn(names);
+        when(statisticsMock.postsCount()).thenReturn(posts);
+        when(statisticsMock.commentsCount()).thenReturn(comments);
 
-        ForumStatistics forumStatistics = new ForumStatistics(statisticsMock)
+        ForumStatistics forumStatistics = new ForumStatistics(statisticsMock);
 
+        //When
+        int postsPerUser = forumStatistics.calculateAdvStatistics().postsPerUser;
+        int commentsPerUser = forumStatistics.calculateAdvStatistics().commentsPerUser;
+        int commentsPerPost = forumStatistics.calculateAdvStatistics().commentsPerPost;
+
+        //Then
+        Assert.assertEquals(0, postsPerUser);
+        Assert.assertEquals(0, commentsPerUser);
+        Assert.assertEquals(0, commentsPerPost);
     }
+
+
+
+
 }
