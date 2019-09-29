ab3a30f3e88dad1debea55a5f083e6b464c09a2c
BartoszekA <agnieszka.bartoszek@gmail.com>
2019-09-29 13:38:41 +0200

    order service first commit

---
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/OrderProcessor.java
@@ -0,0 +1,18 @@
+package com.kodilla.good.patterns.challenges;
+
+public class OrderProcessor {
+
+    private InformationService informationService;
+    private OrderService orderService;
+    private OrderRepository orderRepository;
+
+    public OrderProcessor(final  InformationService informationService,
+                          final OrderService orderService, final OrderRepository orderRepository) {
+        this.informationService = informationService;
+        this.orderService = orderService;
+        this.orderRepository = orderRepository;
+    }
+
+    //public OrderDto
+
+}
---
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/OrderRequest.java
@@ -0,0 +1,10 @@
+package com.kodilla.good.patterns.challenges;
+
+import com.kodilla.good.patterns.srp.User;
+
+public class OrderRequest {
+
+    private User user;
+    private Item item;
+
+}
