4420fd74618daa8d7aa5f5bb1ac7a18f8ede3bd8
BartoszekA <agnieszka.bartoszek@gmail.com>
2019-10-18 23:32:54 +0200

    rps logic changes

--- kodilla-rps/src/main/java/com/kodilla/rps/Round.java
+++ kodilla-rps/src/main/java/com/kodilla/rps/Round.java
@@ -9,22 +9,23 @@ public class Round {
         String userMove;
         int computerMove;
         Scanner scanner = new Scanner(System.in);
+
         for(int i = 0; i < rounds; i++) {
             System.out.println("Wybierz ruch:\n1 - kamień\n2 - papier\n3 - nożyce\nx - koniec gry\nn - nowa gra");
             userMove = scanner.next();
             if(userMove == "x") {
-                System.out.println("Czy na pewno chcesz przerwać grę? (y/n)");
+                System.out.println("Czy na pewno chcesz wyjść z gry? (y/n)");
                 userMove = scanner.next();
                 if(userMove == "y") {
-                    System.out.println("Gra została przewana. Do zobaczenia wkrótce!");
-                    //koniec gry
+                    System.out.println("Gra została zakończona. Do zobaczenia wkrótce!");
+                    System.exit(0);
                 }
                 else if (userMove == "n") {
                     //ciąg dalszy gry
                 }
             }
             else if(userMove == "n"){
-                System.out.println("Czy na pewno chcesz zakończyć grę? (y/n)");
+                System.out.println("Czy na pewno chcesz zakończyć tę rozgrywkę? (y/n)");
                 userMove = scanner.next();
                 if(userMove == "y") {
                     //nowa gra
@@ -38,12 +39,6 @@ public class Round {
                 Random random = new Random();
                 computerMove = random.nextInt(3) + 1;
                 logic.logic(userMove, computerMove);
-            } else {
-                System.out.println("Spróbuj jeszcze raz");
-                userMove = null;
-                scanner = null;
-                move(rounds);
-                //należy zmienić argument, bo zacznie liczyć od nowa ilość rund
             }
         }
     }
--- kodilla-rps/src/main/java/com/kodilla/rps/RpsLogic.java
+++ kodilla-rps/src/main/java/com/kodilla/rps/RpsLogic.java
@@ -1,34 +1,42 @@
 package com.kodilla.rps;
 
 public class RpsLogic {
+    int userPoints = 0;
+    int computerPoints = 0;
 
     public void logic(String userMove, int computerMove) {
         if(userMove == "1" && computerMove == 1) {
-            //tu wpisz cos
+            //remis
         }
         else if(userMove == "1" && computerMove == 2) {
+            computerPoints++;
             //tu wpisz cos
         }
         else if(userMove == "1" && computerMove == 3) {
+            userPoints++;
             //tu wpisz cos
         }
         else if(userMove == "2" && computerMove == 1) {
+            userPoints++;
             //xxx
         }
         else if(userMove == "2" && computerMove == 2) {
-            //xxx
+            //remis
         }
         else if(userMove == "2" && computerMove == 3) {
+            computerPoints++;
             //jjj
         }
         else if(userMove == "3" && computerMove == 1) {
+            computerPoints++;
             //ddd
         }
         else if(userMove == "3" && computerMove == 2) {
+            userPoints++;
             //www
         }
         else if(userMove == "3" && computerMove == 3) {
-            //www
+            //remis
         }
     }
 }
