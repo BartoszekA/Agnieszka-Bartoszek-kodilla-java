da97e5ca9c19a1331137363731c5afe4bac921e1
BartoszekA <agnieszka.bartoszek@gmail.com>
2019-10-25 21:31:14 +0200

    tests

--- kodilla-spring/src/test/java/com/kodilla/spring/calculator/CalculatorTestSuite.java
+++ kodilla-spring/src/test/java/com/kodilla/spring/calculator/CalculatorTestSuite.java
@@ -12,19 +12,46 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class CalculatorTestSuite {
     @Test
-    public void testCalculations() {
+    public void testCalculationsAdd() {
         //Given
         ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring");
         Calculator calculator = context.getBean(Calculator.class);
         //When
         double addResult = calculator.add(2.0, 3.0);
-        double subResult = calculator.sub(16.4, 6.4);
-        double mulResult = calculator.mul(2.3, 3.0);
-        double divResult = calculator.div(8.8, 2.2);
         //Then
         Assert.assertEquals(5.0, addResult, 0.1);
+    }
+
+    @Test
+    public void testCalculationsSub() {
+        //Given
+        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring");
+        Calculator calculator = context.getBean(Calculator.class);
+        //When
+        double subResult = calculator.sub(16.4, 6.4);
+        //Then
         Assert.assertEquals(10.0, subResult, 0.1);
+    }
+
+    @Test
+    public void testCalculationsMul() {
+        //Given
+        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring");
+        Calculator calculator = context.getBean(Calculator.class);
+        //When
+        double mulResult = calculator.mul(2.3, 3.0);
+        //Then
         Assert.assertEquals(6.9, mulResult, 0.1);
+    }
+
+    @Test
+    public void testCalculationsDiv() {
+        //Given
+        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring");
+        Calculator calculator = context.getBean(Calculator.class);
+        //When
+        double divResult = calculator.div(8.8, 2.2);
+        //Then
         Assert.assertEquals(4, divResult, 0.1);
     }
 }
