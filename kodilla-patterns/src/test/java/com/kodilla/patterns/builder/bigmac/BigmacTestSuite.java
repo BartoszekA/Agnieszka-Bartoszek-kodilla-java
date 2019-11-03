39a6d286aa8969d5e43dcfba50453c9f2ba2900f
BartoszekA <agnieszka.bartoszek@gmail.com>
2019-11-03 19:52:00 +0100

    bigmac

---
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/builder/bigmac/BigmacTestSuite.java
@@ -0,0 +1,25 @@
+package com.kodilla.patterns.builder.bigmac;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class BigmacTestSuite {
+    @Test
+    public void testBigmacNew() {
+        //Given
+        Bigmac bigmac = new Bigmac.BigmacBuilder()
+                .bun("sesame")
+                .burgers(1)
+                .sauce("standard")
+                .ingredient("onion")
+                .ingredient("cucumber")
+                .ingredient("tomato")
+                .ingredient("cheese")
+                .build();
+        System.out.println(bigmac);
+        //When
+        int howManyIngredients = bigmac.getIngredients().size();
+        //Then
+        Assert.assertEquals(4, howManyIngredients);
+    }
+}
